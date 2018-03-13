f48a127be6d6265f398858e9b361f6ee4d667370
hugo <hugo86jose@gmail.com>
2018-03-13 15:55:07 -0300

    sql script

---
+++ src/Tables.sql
@@ -0,0 +1,30 @@
+USE [PersonDB]
+GO
+
+/****** Object:  Table [dbo].[Persons]    Script Date: 3/13/2018 3:54:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Persons](
+	[ID] [uniqueidentifier] NOT NULL,
+	[FirstName] [varchar](255) NULL,
+	[LastName] [varchar](255) NULL,
+	[Email] [varchar](255) NULL,
+ CONSTRAINT [PK_Persons] PRIMARY KEY NONCLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
